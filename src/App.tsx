ab67afc597d8a2029b75dc1b4ced6bbad01b3c04
Ronaldo Vitto Lewerissa <vl@Ronaldos-MacBook-Pro.local>
2025-07-05 15:23:17 +0700

    feat: initialize vite project with react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>GitHub User Search</h1>
+      <p>
+        A React application for searching GitHub users and their repositories.
+      </p>
+    </>
+  );
+}
+
+export default App;
