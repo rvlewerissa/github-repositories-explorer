7e5b8375bfefb8062289169868b9e39c4e5db0c4
Ronaldo Vitto Lewerissa <vl@Ronaldos-MacBook-Pro.local>
2025-07-06 16:48:56 +0700

    fix: add full screen height to background in Layout component

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className='bg-gray-50'>
+    <div className='min-h-screen bg-gray-50'>
       <div className='mx-auto max-w-[800px] px-4 py-8 sm:px-6 lg:px-8'>
         {children}
       </div>
